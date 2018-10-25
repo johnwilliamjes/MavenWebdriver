10d0030a774c1babacbee96b0af28c524ad362f9
John <johnwilliamjes@gmail.com>
2018-10-25 00:43:14 -0400

    change cap5

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -40,7 +40,7 @@ public class LoginTest {
 	 FirefoxProfile profile = new FirefoxProfile();
 	 		System.setProperty("webdriver.gecko.driver","C:\\geckodriver\\geckodriver.exe");
 	 		//System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
-	 		//System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "conoleLogs_Firefox.txt");
+	 		System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "conoleLogs_Firefox.txt");
 	 		//firefoxOptions.addPreference("--log", "trace");
 	 		//firefoxOptions.addPreference("browser.popups.showPopupBlocker", false);
 	 		firefoxOptions.addPreference("security.sandbox.content.level", 5);
