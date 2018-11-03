b0de08c795f5291c4437cde3bdf837ba40506a66
John <johnwilliamjes@gmail.com>
2018-11-03 14:02:49 -0400

    commiting thru STS

---
+++ src/test/java/test.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+
+import com.slenium.pageObjcts.Home;
+import com.slenium.pageObjcts.Login;
+
+public class test {
+
+	static WebDriver driver;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Home home =new Home(driver);
+		Login lp = home.GotoLogin();
+		
+		
+
+	}
+
+}
