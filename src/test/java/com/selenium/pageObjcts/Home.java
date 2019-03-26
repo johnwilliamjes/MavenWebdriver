75edbfb6d02769222e37ffda2db0ee3d005eca7a
jjesjava <jjes@git.training>
2019-03-25 22:24:12 -0400

    Home.java and testBase.java adde

---
+++ src/test/java/com/selenium/pageObjcts/Home.java
@@ -0,0 +1,28 @@
+package com.selenium.pageObjcts;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Home extends TestBase {
+
+   
+	public Home(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Login GotoLogin() {
+		// TODO Auto-generated method stub
+		
+		
+		 		
+		
+		return null;
+	}
+
+
+}
---
+++ src/test/java/com/selenium/pageObjcts/TestBase.java
@@ -0,0 +1,29 @@
+package com.selenium.pageObjcts;
+import static org.testng.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+public class TestBase {
+	public static WebDriver driver;
+	public TestBase(WebDriver driver) {
+		
+		this.driver=driver;
+	}
+
+	
+}
