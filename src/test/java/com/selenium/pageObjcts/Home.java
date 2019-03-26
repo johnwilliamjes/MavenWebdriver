9c64426d6d3f3bd16585ce2c2b38feba682f27ed
jjesjava <jjes@git.training>
2019-03-25 22:25:29 -0400

    updated javahome

--- src/test/java/com/selenium/pageObjcts/Home.java
+++ src/test/java/com/selenium/pageObjcts/Home.java
@@ -2,10 +2,6 @@ package com.selenium.pageObjcts;
 
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Home extends TestBase {
 
