69a5a76265fa51052b3b26e741ecb26635bfe08b
jjesjava <jjes@git.training>
2019-03-25 22:18:52 -0400

    remved LoginTest.Java from test/java

---
+++ src/test/java/com/selenium/pageObjcts/Login.java
@@ -0,0 +1,37 @@
+package com.selenium.pageObjcts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Login extends TestBase {
+	
+	
+	public Login(WebDriver driver) {
+		super(driver);
+	}
+
+	
+	public static void doLogin() {
+		 DesiredCapabilities desiredCapabilities = DesiredCapabilities.firefox();
+		 final FirefoxOptions firefoxOptions = new FirefoxOptions(desiredCapabilities);
+		 FirefoxProfile profile = new FirefoxProfile();
+		 		System.setProperty("webdriver.gecko.driver","C:\\geckodriver\\geckodriver.exe");
+		 		//System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
+		 		System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "conoleLogs_Firefox.txt");
+		 		//firefoxOptions.addPreference("--log", "trace");
+		 		//firefoxOptions.addPreference("browser.popups.showPopupBlocker", false);
+		 		firefoxOptions.addPreference("security.sandbox.content.level", 5);
+		 		firefoxOptions.setAcceptInsecureCerts(true);
+		 		firefoxOptions.setProfile(profile);
+		 		driver = new FirefoxDriver(firefoxOptions);
+	}
+
+	
+	public static void logoff() {
+		
+		driver.quit();
+	}
+}
