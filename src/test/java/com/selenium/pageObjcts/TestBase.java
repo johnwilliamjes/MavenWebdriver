79bfc281e4c95c8e699c4dedf4a6694b6d4e78a3
jjesjava <jjes@git.training>
2019-03-25 22:26:56 -0400

    updated

--- src/test/java/com/selenium/pageObjcts/TestBase.java
+++ src/test/java/com/selenium/pageObjcts/TestBase.java
@@ -1,21 +1,6 @@
 package com.selenium.pageObjcts;
-import static org.testng.Assert.assertEquals;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
 
 
 public class TestBase {
