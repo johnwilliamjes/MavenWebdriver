be22b322c30c8d85f213ff6212803b076fd04b17
John <johnwilliamjes@gmail.com>
2021-06-11 08:14:10 -0400

    updated by git1

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -15,8 +15,8 @@ public class test {
 	public static void main(String[] args) {
 		//MyChange
 
-		Home home =new Home(driver);
-		Login lp = home.GotoLogin();
+		Home home1 =new Home(driver);
+		Login lp = home1.GotoLogin();
 		
 		
 
